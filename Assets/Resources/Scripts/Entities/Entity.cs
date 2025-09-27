4fb58153f86a28e0e39b97a48566e922b840a007
myaltaccountsthis <71832770+myaltaccountsthis@users.noreply.github.com>
2025-09-26 22:18:45 -0500

    Create file structure

---
+++ Assets/Resources/Scripts/Entities/Entity.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    public bool IsFrozen { get; private set; }
+
+    protected virtual void Awake()
+    {
+
+    }
+
+    protected virtual void Start()
+    {
+
+    }
+
+    protected virtual void Update()
+    {
+
+    }
+}
---
+++ Assets/Resources/Scripts/Entities/ReversibleEntity.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class ReversibleEntityData
+{
+    public readonly Vector3 position;
+
+    public ReversibleEntityData(Vector3 pos)
+    {
+        position = pos;
+    }
+
+    // Subclasses must cast to the respective Entity type in the Apply method
+    public virtual void Apply(ReversibleEntity entity)
+    {
+        entity.transform.position = position;
+    }
+}
+
+public class ReversibleEntity : Entity
+{
+    private const int MAX_STATE_HISTORY = 1200;
+    private readonly LinkedList<ReversibleEntityData> stateHistory = new();
+
+    public int ReverseTime { get; private set; } = 0;
+    public bool IsReversing => ReverseTime > 0;
+
+    protected override void Update()
+    {
+        base.Update();
+
+        if (IsReversing)
+        {
+            ReverseTime--;
+            if (stateHistory.Count > 0)
+            {
+                // TODO change this to not be linear
+                // Apply the last state and remove it from history
+                ReversibleEntityData lastState = stateHistory.Last.Value;
+                lastState.Apply(this);
+                stateHistory.RemoveLast();
+            }
+            // Don't record state if reversing
+            return;
+        }
+
+        // Add current state to history
+        stateHistory.AddLast(CaptureState());
+        // Remove first state if we exceed max history size
+        while (stateHistory.Count > MAX_STATE_HISTORY)
+        {
+            stateHistory.RemoveFirst();
+        }
+    }
+
+    protected virtual ReversibleEntityData CaptureState()
+    {
+        return new ReversibleEntityData(transform.position);
+    }
+}
