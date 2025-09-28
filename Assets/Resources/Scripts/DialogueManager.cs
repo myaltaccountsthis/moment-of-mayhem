dd526f27f7839dd18fb15fc7b2635f4c18911dbe
Sahasrad Chippa <52544077+MetaFlash@users.noreply.github.com>
2025-09-28 18:01:36 -0400

    Add dialogue to present

--- Assets/Resources/Scripts/DialogueManager.cs
+++ Assets/Resources/Scripts/DialogueManager.cs
@@ -4,58 +4,51 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-
 public class DialogueManager : MonoBehaviour, IPointerClickHandler
 {
-    enum TypingState
-    {
-        Typing,
-        Finishing,
-        Stopping
-    }
+    enum TypingState { Typing, Finishing, Stopping }
 
-    class TypingInfo
-    {
-        public TypingState state;
-    }
+    class TypingInfo { public TypingState state; }
     
     public TextMeshProUGUI header;
     public TextMeshProUGUI body;
-    public Transform inactivePos;
+    public Transform inactivePos;   // must share the same parent as `slate`
     public GameObject slate;
 
-    private Vector3 activePos;
+    private Vector3 activeLocalPos;
     private TypingInfo lastInfo;
-    
+
     public void Awake()
     {
-        activePos = slate.transform.position;
-        gameObject.SetActive(false);
-        
-        slate.transform.position = inactivePos.position;
+        activeLocalPos = slate.transform.localPosition;
+
+        slate.SetActive(false);
+        slate.transform.localPosition = inactivePos.localPosition;
+
         header.text = "";
         body.text = "";
-        // temporary
-        ShowDialogue("Hey", "yo yo yo yo yo yo yo yo yo");
-        Debug.Log("Showing dialogue");
     }
-    
+
     public void ShowDialogue(string headerText, string bodyText)
     {
         LeanTween.cancel(slate);
+
         if (lastInfo != null)
-        {
             lastInfo.state = TypingState.Stopping;
-        }
+
         lastInfo = new TypingInfo { state = TypingState.Typing };
-        slate.transform.position = inactivePos.position;
-        gameObject.SetActive(true);
+
+        slate.transform.localPosition = inactivePos.localPosition;
+        slate.SetActive(true);
+
         header.text = headerText;
         StartCoroutine(TypeText(bodyText, body, 0.05f, lastInfo));
-        LeanTween.move(slate, activePos, 1f).setEaseInOutSine();
+
+        LeanTween.moveLocal(slate, activeLocalPos, 1f).setEaseInOutSine();
     }
 
-    IEnumerator TypeText(string fullText, TextMeshProUGUI textComponent, float delay, TypingInfo info) {
+    IEnumerator TypeText(string fullText, TextMeshProUGUI textComponent, float delay, TypingInfo info)
+    {
         textComponent.text = "";
         for (int i = 0; i < fullText.Length; i++)
         {
@@ -71,37 +64,36 @@ public class DialogueManager : MonoBehaviour, IPointerClickHandler
             textComponent.text += fullText[i];
             yield return new WaitForSeconds(delay);
         }
-        
+
         if (info.state == TypingState.Typing)
-        {
             info.state = TypingState.Finishing;
-        }
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        if (lastInfo != null)
+        if (lastInfo == null) return;
+
+        if (lastInfo.state == TypingState.Typing)
         {
-            if (lastInfo.state == TypingState.Typing)
-            {
-                lastInfo.state = TypingState.Finishing;
-            }
-            else if (lastInfo.state == TypingState.Finishing)
-            {
-                lastInfo.state = TypingState.Stopping;
-                HideDialogue();
-            }
+            lastInfo.state = TypingState.Finishing;
+        }
+        else if (lastInfo.state == TypingState.Finishing)
+        {
+            lastInfo.state = TypingState.Stopping;
+            HideDialogue();
         }
     }
 
     public void HideDialogue()
     {
         LeanTween.cancel(slate);
-        LeanTween.move(slate, inactivePos.position, 1f).setEaseInOutSine().setOnComplete(() =>
-        {
-            gameObject.SetActive(false);
-            header.text = "";
-            body.text = "";
-        });
+        LeanTween.moveLocal(slate, inactivePos.localPosition, 1f)
+            .setEaseInOutSine()
+            .setOnComplete(() =>
+            {
+                slate.SetActive(false);
+                header.text = "";
+                body.text = "";
+            });
     }
 }
