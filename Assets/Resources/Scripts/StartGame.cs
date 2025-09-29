b28e2dcc70980909fef9a9e7afc50dc744105ba9
myaltaccountsthis <71832770+myaltaccountsthis@users.noreply.github.com>
2025-09-28 21:39:14 -0500

    Fixes

--- Assets/Resources/Scripts/StartGame.cs
+++ Assets/Resources/Scripts/StartGame.cs
@@ -1,11 +1,10 @@
-using UnityEditor;
 using UnityEngine;
 
 public class StartGame : MonoBehaviour
 {
-    [SerializeField] private SceneAsset firstLevel;
+    [SerializeField] private string firstLevel;
     public void StartTheGame()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene(firstLevel.name);
+        UnityEngine.SceneManagement.SceneManager.LoadScene(firstLevel);
     }
 }
